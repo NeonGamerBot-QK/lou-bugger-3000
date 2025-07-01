ce05b7f8e712e326c107c7810e11ab334ebc5652
Saahil <neon@saahild.com>
2025-07-01 15:31:49 -0400

    feat(tree-wide): init

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { app,BrowserWindow } from "electron";
+    let window;
+
+function createWindow() {
+    if(window) return;
+    window = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    window.loadFile("index.html");
+}
+
+app.on("ready", () => {
+createWindow()
+})
+
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
+    }
+})
